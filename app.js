90390dcc3de5d251f604444aed9de26716579657
revenuegenerator <Joymes@plooze.com>
2021-05-17 21:57:04 +0900

    RPS2

---
+++ app.js
@@ -0,0 +1,10 @@
+computerPlay(){
+    let random = Math.random()
+    if (random < .3) { console.log("Rock")}
+    if (random > .3 && random < .6) {console.log("Paper")}
+}
+
+
+
+
+console.log('hello world')
