29a28815415a174e2f96a3dffc430fd3121df683
revenuegenerator <Joymes@plooze.com>
2021-05-17 21:57:19 +0900

    RPS2

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-computerPlay(){
-    let random = Math.random()
-    if (random < .3) { console.log("Rock")}
-    if (random > .3 && random < .6) {console.log("Paper")}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    }
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Paper beats Rock, Computer Wins";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock beats Scissors, Player Wins";
+    }
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Paper beats Rock, Player Wins";
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Scissors beats Paper, Computer Wins";
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Rock beats Scissors, Computer Wins";
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Scissors beats Paper, Player Wins";
+    }
+  }
+
+
+function computerPlay(){
+    const computerOption = ["rock", "paper", "scissors"];
+    const computerSelection = Math.floor(Math.random() * computerOption.length);
 }
 
 
-
-
-console.log('hello world')
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  
+  console.log(playRound(playerSelection, computerSelection));
